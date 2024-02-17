de716dfb62655f0d7c9092d0487e7bb3d8c35cd0
Rahaf0B <rahafbakeer123@gmail.com>
2024-02-17 22:58:05 +0200

    update to check deployment

--- controller/product.ts
+++ controller/product.ts
@@ -29,8 +29,11 @@ export default class CProduct {
           limit: ItemNumber,
           sort: { _id: 1 },
         });
+      console.log(await data.toArray()); 
       return data.toArray() as unknown as IProduct[];
+
     } catch (e: any) {
+      console.error(e);
       throw new Error(e.message);
     }
   }
